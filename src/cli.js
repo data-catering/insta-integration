5b542283017bc77a0bde701b6fdf65e6f1b34b28
Peter Flook <pflooky@hotmail.com>
2025-01-28 15:06:15 +0800

    Upgrade 0.14 (#7)
    * Update data-caterer to 0.14.5

--- src/cli.js
+++ src/cli.js
@@ -21,7 +21,7 @@ program
   .option(
     '-d, --data-caterer-version <version>',
     'Version of data-caterer Docker image',
-    '0.14.3'
+    '0.14.5'
   )
   .option('-u, --data-caterer-user <user>', 'User for data-caterer', '')
   .option('-t, --data-caterer-token <token>', 'Token for data-caterer', '')
